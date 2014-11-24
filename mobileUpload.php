2fd840eccdf636b074ecb67aadf189f7cc2de5b8
Hector Sandoval <hesandova@gmail.com>
2014-11-24 14:46:19 -0800

    added email response to mobileUpload

--- mobileUpload.php
+++ mobileUpload.php
@@ -67,6 +67,33 @@
         {
             echo "Completed!";
             http_response_code(200);
+            $email = $_REQUEST['email'] ;
+            $subject = "Alert! A new pulse has been posted to SharkPulse";
+            $header  = 'MIME-Version: 1.0' . "\r\n";
+            $header .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+            $header .= 'To: Edgar <edsan@example.com>' . "\r\n";
+            $header .= 'From: SharkPulse <sharkbaselines@example.com>' . "\r\n";
+
+            $message = "
+                <html> 
+                <head>
+                    <tite>Shark Pulse Alert</title>
+                </head>
+                <body>
+                    <h1>Pulse Information</h1><br>
+                    <h2>Species guessed: A Species</h2><br>
+                    <h2>Latitude: $users_information['LATITUDE']</h2><br>
+                    <h2>Longitude: $users_information['LONGITUDE']</h2><br>
+                    <h2>Date: $users_information['DATE']</h2><br>
+                    <h2>Posted by: <a href=$email> $users_information['USERS_EMAIL']</a></h2><br>
+                </body>
+                </html>
+            ";
+
+  if(mail( "edsan5678@sbcglobal.net", $subject,
+    $message, $header )){
+    echo "Mail Sent Successfully";
+    }
         }
         else{
             header("HTTP/1.0 404 Not Found");
